f914bba232987c3aeb9e773ea1a0dceb06b51b91
HuMiao <humiao2006@gmail.com>
2025-12-21 10:59:13 +0800

    Initial Commit

---
+++ RideFare/RideFareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideFareApp.swift
+//  RideFare
+//
+//  Created by Miao on 2025/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RideFareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
